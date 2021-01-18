e065aa5477b625393074f28888f1433520039484
Oleh Mazur <real.trolforever@gmail.com>
2021-01-18 14:48:15 +0200

    Add necessary apt packages installation

---
+++ System Setup/jumpcloud_setup.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+
+echo ($1)
+
+sudo apt install apt-rdepends \
+apt-show-versions \
+coreutils \
+curl \
+dpkg \
+grep \
+hostname \
+libc-bin \
+lsb-release \
+lsof \
+mawk \
+passwd \
+procps \
+sysvinit-utils \
+tar \
+sudo \ 
+gdebi-core \
+libnss3 \
+libnss3-tools -y
