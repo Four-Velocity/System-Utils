e329d49d8755b4341015302e7adf6d11bb32763c
Oleh Mazur <real.trolforever@gmail.com>
2021-01-18 15:24:20 +0200

    Fix space error; Hide echo from tee

--- System Setup/jumpcloud_setup.sh
+++ System Setup/jumpcloud_setup.sh
@@ -4,12 +4,12 @@ jumpcloud_key=$1
 original_issue=$(cat /etc/issue)
 supported_issue="Ubuntu 20.04.1 LTS \n \l"
 
-sudo apt install apt-rdepends apt-show-versions coreutils\
+sudo apt install apt-rdepends apt-show-versions coreutils \
 curl sudo dpkg grep hostname libc-bin lsb-release lsof mawk passwd \
 procps sysvinit-utils tar sudo gdebi-core libnss3 libnss3-tools -y
 
-echo "$supported_issue" | sudo tee /etc/issue
+echo "$supported_issue" | sudo tee /etc/issue 1>/dev/null
 
 # curl --tlsv1.2 --silent --show-error --header "x-connect-key: $jumpcloud_key" https://kickstart.jumpcloud.com/Kickstart | sudo bash
 
-echo "$original_issue" | sudo tee /etc/issue
+echo "$original_issue" | sudo tee /etc/issue 1>dev/null
